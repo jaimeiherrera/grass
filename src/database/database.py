16e4e7ebb8b0f5bfb87b2962a95500af31aa35e4
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2022-07-20 19:06:18 -0400

    Cambios a ser confirmados: 	modificado:     .gitignore 	modificado:     requirements.txt 	modificado:     src/database/database.py

--- src/database/database.py
+++ src/database/database.py
@@ -1,17 +1,13 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
-from sqlalchemy.sql import func
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from .database import Base
+SQLALCHEMY_DATABASE_URL = "sqlite:///./grass.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class Persona(Base):
-    __tablename__ = "personas"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(length=20), nullable=False)
-    email = Column(String(length=30), unique=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True),
-                        onupdate=func.now(), server_default=func.now())
+Base = declarative_base()
